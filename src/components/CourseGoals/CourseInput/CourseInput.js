023141cf182c83138c357bfc46cb69038a59e060
Phoenix <adit.parida1@gmail.com>
2021-05-10 15:05:19 -0700

    Updated for modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,48 +1,48 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-//import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 
-const FormControl = styled.div `
+// const FormControl = styled.div `
 
 
-margin: 0.5rem 0;
+// margin: 0.5rem 0;
 
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid #ccc;
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
 
-&.invalid input
-{
-  border-color: red;
-  background: rgb(238, 140, 140);
-}
+// &.invalid input
+// {
+//   border-color: red;
+//   background: rgb(238, 140, 140);
+// }
 
-&.invalid label
-{
-  color: red;
-}
-`;
+// &.invalid label
+// {
+//   color: red;
+// }
+// `;
 
 
 const CourseInput = props => {
@@ -71,13 +71,13 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* <div className={"form-control" + (!isValid ? ' invalid' : '')}> */}
-      {/* <div className={`form-control ${!isValid ? 'invalid': ''}`}> */}
-      <FormControl className={!isValid && 'invalid'}>
+      <div className={`${styles['form-control']} ${!isValid ? styles.invalid : ''}`}>
+      {/* <FormControl className={!isValid && 'invalid'}> */}
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      {/* </FormControl> */}
         
-      {/* </div> */}
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
